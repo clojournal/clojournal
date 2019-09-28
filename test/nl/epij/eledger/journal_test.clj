b8d44037ddd2e2f0bb4567275e9d2bd8207cb236
Pepijn Looije <p@epij.nl>
2019-09-28 16:10:38 -0300

    Add test for journal generation

---
+++ test/nl/epij/eledger/journal_test.clj
@@ -0,0 +1,33 @@
+(ns nl.epij.eledger.journal-test
+  (:require [clojure.test :refer :all]
+            [clojure.spec.test.alpha :as stest]
+            [nl.epij.eledger.api :as api]
+            [clojure.spec.gen.alpha :as gen]
+            [clojure.spec.alpha :as s]
+            [nl.epij.eledger :as eledger]
+            [clojure.string :as str]))
+
+(deftest journal-generation
+  (is (= (api/journal [{::eledger/date           "2019-06-01"
+                        ::eledger/transaction-id #uuid "960e3e3d-1d5d-45d9-aa92-50f7bfcd2efc"
+                        ::eledger/payee          "Mister Shawarma"
+                        ::eledger/postings       [{::eledger/account :expenses/food
+                                                   ::eledger/amount  "R$ 20"}
+                                                  {::eledger/account :assets/cash}]}
+                       {::eledger/date     #time/date "2019-07-01"
+                        ::eledger/payee    "Interactive Brokers"
+                        ::eledger/postings [{::eledger/account :assets/stocks
+                                             ::eledger/amount  "USD 1336"}
+                                            {::eledger/account :expenses/commissions
+                                             ::eledger/amount  "USD 1"}
+                                            {::eledger/account :assets/checking}]}])
+         (str/ltrim "
+2019-06-01 (960e3e3d-1d5d-45d9-aa92-50f7bfcd2efc) Mister Shawarma
+  :expenses/food  R$ 20
+  :assets/cash
+
+2019-07-01 Interactive Brokers
+  :assets/stocks  USD 1336
+  :expenses/commissions  USD 1
+  :assets/checking
+"))))
